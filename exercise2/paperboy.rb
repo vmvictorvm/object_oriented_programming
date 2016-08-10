0819ec0abb79eeb8694eff11ab90afa8c891ce32
Victor McLean <vmvictorvm@gmail.com>
2016-08-10 16:22:48 -0400

    added deliver method

--- exercise2/paperboy.rb
+++ exercise2/paperboy.rb
@@ -14,7 +14,27 @@ class Paperboy
   def quota
     return @experience/2 + 50
   end
+
+  def deliver(start_address, end_address)
+    if start_address <= end_address
+      i = start_address
+      total = 0
+      while i <= end_address do
+        if i <= quota
+          total += 0.25
+        elsif i > quota
+          total += 0.5
+        end
+        i += 2
+      end
+      return total
+    else
+      puts "Error: Start address cannot be greater than end address"
+    end
+  end
+
 end
 
 pBoy1 = Paperboy.new("David", 80, "even", 80)
 puts pBoy1.quota
+puts pBoy1.deliver(66,66)
