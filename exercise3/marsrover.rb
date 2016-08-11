f7c8c31e5fcc13c195734a4036068cb9a8b69a9a
Victor McLean <vmvictorvm@gmail.com>
2016-08-11 16:20:41 -0400

    exercise3 finished - change the input method

--- exercise3/marsrover.rb
+++ exercise3/marsrover.rb
@@ -13,12 +13,12 @@ class MarsRover
   end
 
 
-  def read_instruction
-    puts "Please enter your instructions: L = Left, R = Right and M = Move"
+  def read_instruction(inputArray)
+    #puts "Please enter your instructions: L = Left, R = Right and M = Move"
 
     # Converting string to array
-    inputString = gets.chomp
-    inputArray =  inputString.split("")
+    #inputString = gets.chomp
+    #inputArray =  inputString.split("")
 
     inputArray.each do |i|
       if i == "L"
@@ -83,19 +83,67 @@ class MarsRover
 
 end
 
-# Getting plateau size input from user in this format "5 5"
-puts "Please enter plateau size. e.g: 5 5"
-plateauSizeInput = gets.chomp
-plateauSizeArray =  plateauSizeInput.split(" ")
 
-puts "Please enter initial rover location"
-initRoverLocationInput = gets.chomp
-initRoverLocationInputArray = initRoverLocationInput.split(" ")
+puts "Please provide user input.  Type 'END' to ended your input"
+$/ = "END"
+user_input = STDIN.gets
+#raise user_input.inspect
+#raise user_input.each_line.inspect
+i =0
+user_input.split("\n").each do |line|
+  next if line == "END"
+    #puts line.inspect
+    i += 1
+    if i == 1  #If it is the very first line, then this is the plateau size
+      plateauSizeInput = line
+      @plateauSizeArray =  plateauSizeInput.split(" ")
+    elsif i == 2  #If even # --> Current position of the rover
+      initRoverLocationInput = line
+      @initRoverLocationInputArray = initRoverLocationInput.split(" ")
+    elsif i == 3 # odd # ----> This line is the instructions for the rover
+      inputString = line
+      @inputArray =  inputString.split("")
+    elsif i == 4
+      initRoverLocationInput = line
+      @initRoverLocationInputArray2 = initRoverLocationInput.split(" ")
+    elsif i == 5
+      inputString = line
+      @inputArray2 =  inputString.split("")
+    end
+end
 
-rover1 = MarsRover.new(initRoverLocationInputArray[0].to_i,initRoverLocationInputArray[1].to_i,initRoverLocationInputArray[2])
+rover1 = MarsRover.new(@initRoverLocationInputArray[0].to_i,@initRoverLocationInputArray[1].to_i,@initRoverLocationInputArray[2])
 #Immediately after create the object, set the plateau size for max x and y coordinate
-rover1.xMax = plateauSizeArray[0].to_i
-rover1.yMax = plateauSizeArray[1].to_i
+rover1.xMax = @plateauSizeArray[0].to_i
+rover1.yMax = @plateauSizeArray[1].to_i
+rover1.read_instruction(@inputArray)
 
-rover1.read_instruction
-puts "The rover1 current direction is #{rover1.xCoord} #{rover1.yCoord} #{rover1.direction}"
+puts "Rover 1: #{rover1.xCoord} #{rover1.yCoord} #{rover1.direction}"
+
+
+rover2 = MarsRover.new(@initRoverLocationInputArray2[0].to_i,@initRoverLocationInputArray2[1].to_i,@initRoverLocationInputArray2[2])
+#Immediately after create the object, set the plateau size for max x and y coordinate
+rover2.xMax = @plateauSizeArray[0].to_i
+rover2.yMax = @plateauSizeArray[1].to_i
+rover2.read_instruction(@inputArray2)
+
+puts "Rover 2: #{rover2.xCoord} #{rover2.yCoord} #{rover2.direction}"
+
+
+### Alternate input ###
+# Getting plateau size input from user in this format "5 5"
+#puts "Please enter plateau size. e.g: 5 5"
+#plateauSizeInput = gets.chomp
+#plateauSizeArray =  plateauSizeInput.split(" ")
+
+#puts "Please enter initial rover location"
+#initRoverLocationInput = gets.chomp
+#initRoverLocationInputArray = initRoverLocationInput.split(" ")
+
+#rover1 = MarsRover.new(initRoverLocationInputArray[0].to_i,initRoverLocationInputArray[1].to_i,initRoverLocationInputArray[2])
+#Immediately after create the object, set the plateau size for max x and y coordinate
+#rover1.xMax = plateauSizeArray[0].to_i
+#rover1.yMax = plateauSizeArray[1].to_i
+
+#rover1.read_instruction
+#puts "The rover1 current direction is #{rover1.xCoord} #{rover1.yCoord} #{rover1.direction}"
